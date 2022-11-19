cb24d7677592468355cb842cfba2795a303ffe80
jentz2909 <65669574+jentz2909@users.noreply.github.com>
2022-11-20 01:02:45 +0800

    add reducer

---
+++ src/store/actions/stockAction.ts
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import { Dispatch } from 'redux'
+import * as actionTypes from '../actionTypes'
+
+export const getAllSnacks = (): any => {
+    return async (dispatch: Dispatch) => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_HOST ?? ''}/stock/` ?? '')
+
+            dispatch({
+                type: actionTypes.ALL_SNACKS,
+                data: response.data
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
